a03309caf3ca5c32e921b111459fefaab649dc80
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2021-10-06 14:50:27 -0300

    create Authors model

--- bookshub/books/models.py
+++ bookshub/books/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return f"{self.name}, ID: {self.id}"
+
+
 class Genre(models.Model):
     title = models.CharField(max_length=200)
 
     def __str__(self) -> str:
         return f"{self.title}, ID: {self.id}"
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -12,6 +21,7 @@ class Book(models.Model):
     isbn10 = models.CharField(max_length=10, blank=True)
     isbn13 = models.CharField(max_length=13, blank=True)
     genres = models.ManyToManyField(Genre, related_name="books", blank=True)
+    authors = models.ManyToManyField(Author, related_name="authors", blank=True)
 
     def __str__(self) -> str:
         return f"{self.title}, ID: {self.id}"
