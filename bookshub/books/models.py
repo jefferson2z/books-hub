3e3ba6276ccc83ba1ef60b6bec73fc11b678ef5a
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2021-10-05 21:17:31 -0300

    create Book/Genre models

--- bookshub/books/models.py
+++ bookshub/books/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return f"{self.title}, ID: {self.id}"
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    isbn10 = models.CharField(max_length=10, blank=True)
+    isbn13 = models.CharField(max_length=13, blank=True)
+    genres = models.ManyToManyField(Genre, related_name="books", blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.title}, ID: {self.id}"
