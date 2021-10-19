36fdc8ea783c45f704c597e271a8eaaa8cd558cc
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2021-10-19 10:43:28 -0300

    update test_uses_genre_form_template

--- bookshub/books/tests.py
+++ bookshub/books/tests.py
@@ -115,4 +115,4 @@ class BooksListViewTest(TestCase):
 class GenreFormTest(TestCase):
     def test_uses_genre_form_template(self):
         response = self.client.get("/genres/new/")
-        self.assertTemplateUsed(response, "books/genre_form.html")
+        self.assertTemplateUsed(response, "create_form.html")
