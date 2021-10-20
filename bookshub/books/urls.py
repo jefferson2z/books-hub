921bd884a54a509f3065d30403249416fc6042c2
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2021-10-20 16:45:32 -0300

    use reverse resolution for urls on templates

--- bookshub/books/urls.py
+++ bookshub/books/urls.py
@@ -4,10 +4,10 @@ from books import views
 
 app_name = "books"
 urlpatterns = [
-    path("genres/<int:genre_id>/", views.genre_details, name="genre_details"),
-    path("genres/new/", views.genre_form, name="genre_form"),
-    path("books/<int:book_id>/", views.book_details, name="book_details"),
-    path("books/new/", views.book_form, name="book_form"),
-    path("books/", views.books_list, name="books_list"),
-    path("", views.genres_list, name="genres_list"),
+    path("genres/<int:genre_id>/", views.genre_details, name="genre-details"),
+    path("genres/new/", views.genre_form, name="new-genre-form"),
+    path("books/<int:book_id>/", views.book_details, name="book-details"),
+    path("books/new/", views.book_form, name="new-book-form"),
+    path("books/", views.books_list, name="books-list"),
+    path("", views.genres_list, name="genres-list"),
 ]
