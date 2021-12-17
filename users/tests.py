39f0cd99df8243f778c8690cb043f38b0f9279e3
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2021-12-16 21:51:35 -0300

    remove hard coded urls from tests

--- users/tests.py
+++ users/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class UserLoginView(TestCase):
     def test_uses_login_template(self):
-        response = self.client.get('/users/login/')
+        response = self.client.get(
+            reverse('users:user-login'))
         self.assertTemplateUsed(response, 'users/login.html')
