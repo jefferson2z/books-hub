5140a53279f28978a9a54853b954a69b529f79fe
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2021-12-16 21:09:39 -0300

    remove hard coded urls from test_views

--- books/tests/test_views.py
+++ books/tests/test_views.py
@@ -1,16 +1,18 @@
 
 from django.test import TestCase
+from django.urls import reverse
+
 from books.models import Book, Genre
 
 
 class GenresListViewTest(TestCase):
     def test_uses_genres_list_template(self):
-        response = self.client.get("/")
+        response = self.client.get(reverse('books:genres-list'))
         self.assertTemplateUsed(response, "books/genres_list.html")
 
     def test_displays_existing_genre(self):
         Genre.objects.create(title="Fantasy")
-        response = self.client.get("/")
+        response = self.client.get(reverse('books:genres-list'))
 
         self.assertContains(response, "Fantasy")
 
@@ -18,12 +20,14 @@ class GenresListViewTest(TestCase):
 class GenreDetailsViewTest(TestCase):
     def test_uses_genre_details_template(self):
         genre = Genre.objects.create(title="Fantasy")
-        response = self.client.get(f"/genres/{genre.id}/")
+        response = self.client.get(
+            reverse('books:genre-details', args=[genre.id]))
         self.assertTemplateUsed(response, "books/genre_details.html")
 
     def test_display_correct_genre(self):
         genre = Genre.objects.create(title="Fantasy")
-        response = self.client.get(f"/genres/{genre.id}/")
+        response = self.client.get(
+            reverse("books:genre-details", args=[genre.id]))
 
         self.assertContains(response, "Fantasy")
         self.assertContains(response, "Genre Fantasy")
@@ -36,7 +40,9 @@ class GenreDetailsViewTest(TestCase):
         first_book.save()
         first_book.genres.add(first_genre)
 
-        response = self.client.get(f"/genres/{first_genre.id}/")
+        response = self.client.get(
+            reverse('books:genre-details', args=[first_genre.id])
+        )
 
         self.assertContains(response, "Fantasy")
         self.assertContains(response, "Lord of the Rings")
@@ -44,11 +50,11 @@ class GenreDetailsViewTest(TestCase):
 
 class BooksListViewTest(TestCase):
     def test_uses_books_list_template(self):
-        response = self.client.get("/books/")
+        response = self.client.get(reverse('books:books-list'))
         self.assertTemplateUsed(response, "books/books_list.html")
 
     def test_displays_existing_book(self):
         Book.objects.create(title="Les Miserables")
-        response = self.client.get("/books/")
+        response = self.client.get(reverse('books:books-list'))
 
         self.assertContains(response, "Les Miserables")
