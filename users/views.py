7ce2c179eb888c22f36333ea2ff6ceae11eed581
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2021-12-19 00:12:12 -0300

    handle creating a user

--- users/views.py
+++ users/views.py
@@ -1,5 +1,20 @@
+from django.http.response import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
 from django.shortcuts import render
+
+from users.forms import CreateUserForm
 
 
 def create_user(request):
-    return render(request, 'users/create_user.html')
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = authenticate(
+                username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
+            login(request, user)
+            return HttpResponseRedirect(reverse("/"))
+    else:
+        form = CreateUserForm()
+    return render(request, 'users/create.html', {"form": form})
