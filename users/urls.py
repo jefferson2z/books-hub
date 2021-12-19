30da07dcba5583cec1c250ffd63daa50ed102904
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2021-12-18 23:03:13 -0300

    hanlde user login

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from users import views
 
 app_name = "users"
 urlpatterns = [
-    path("login/", views.user_login, name="user-login"),
+    path("login/", auth_views.LoginView.as_view(template_name='users/login.html',
+         next_page='/'), name="user-login"),
     path("create/", views.create_user, name="create-user")
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 
 
-def user_login(request):
-    return render(request, 'users/login.html')
-
-
 def create_user(request):
     return render(request, 'users/create_user.html')
